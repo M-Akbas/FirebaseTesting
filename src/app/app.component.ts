7954bbbdda5f4708fd6bea17b9c48333f85e3125
Muhammed Akbas <md.akbas@gmx.de>
2023-11-24 16:45:43 +0100

    array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  allUsers: any = [];
   title = 'FirebaseApp';
+  firestore: Firestore = inject(Firestore);
+  counter: number = 0;
+  nameOfUser!:string; 
+  
+  constructor() {
+    this.getUserFromFirestore();
+  }
+
+
+  async getUserFromFirestore() {
+    this.allUsers = [];
+    const usersRef = collection(this.firestore, "allUser");
+    const querySnapshot = await getDocs(usersRef);
+    querySnapshot.forEach((doc) => {
+      this.allUsers.push(doc.data());
+    })
+    this.showNameOfUser();
+  };
+
+  showNameOfUser() {
+    for (let i = 0; i < this.allUsers.length; i++) {
+      const userName = this.allUsers[i].name;
+      console.log(userName)
+      this.nameOfUser = userName;
+      
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"simplefirebaseap","appId":"1:760073860350:web:a2a96fda79ec743faa21af","storageBucket":"simplefirebaseap.appspot.com","apiKey":"AIzaSyAlgIcqypJZX3wJUj7BRvynOhOVuiGv_P4","authDomain":"simplefirebaseap.firebaseapp.com","messagingSenderId":"760073860350"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
